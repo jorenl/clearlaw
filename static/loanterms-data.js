7bf386218c6e14b664ceee92bf9a2e7b7b65aa4b
Robert <robertm@Roberts-MacBook-Pro-2.local>
2018-11-11 07:35:29 -0500

    new data

--- static/loanterms-data.js
+++ static/loanterms-data.js
@@ -2,9 +2,9 @@ var loans = [
 		{ 	
 		"source":"checkLoan", 
 		"url":"n/a", 
-		"interest":15, 
+		"interest":7, 
 		"term":5, 
-		"totalAmount":30, 
+		"totalAmount":29.70, 
 		"predScore":8
 	},
 	{
@@ -12,23 +12,23 @@ var loans = [
 		"url": "https://studentaid.ed.gov/sa/types/loans/interest-rates", 
 		"interest":5.05, 
 		"term":5, 
-		"totalAmount":30, 
+		"totalAmount":28.7, 
 		"predScore":3
 	},
 	{
 		"source":"SunTrust",
 		"url":"https://www.suntrust.com/loans/student-loans", 
-		"interest":6.05, 
-		"term":15, 
-		"totalAmount":30, 
+		"interest":6, 
+		"term":12, 
+		"totalAmount":35, 
 		"predScore":4
 	},
 	{ 
 		"source":"Discover",
 	 	"url":"http://discover.com/student-loans/undergraduate.html",
-	 	"interest":4.05, 
-	 	"term":7, 
-	 	"totalAmount":30, 
+	 	"interest":11, 
+	 	"term":5, 
+	 	"totalAmount":32.61, 
 	 	"predScore":6
 	},
 	{ 
@@ -36,7 +36,7 @@ var loans = [
 		"url":"https://www.salliemae.com/student-loans/",
 		"interest":8.05,
 		"term":9,
-		"totalAmount":30,
+		"totalAmount":35.15,
 		"predScore":8
 	}
 
